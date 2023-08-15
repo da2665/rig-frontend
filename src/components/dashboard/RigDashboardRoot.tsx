b44a5c18dd94bbc98e167f08dc7245788f29cac0
Dylan Armstrong <dylanarmstrong8998@gmail.com>
2023-08-15 19:34:33 +1000

    Added useEffect to get updates once Dashboard loads

--- src/components/dashboard/RigDashboardRoot.tsx
+++ src/components/dashboard/RigDashboardRoot.tsx
@@ -1,8 +1,19 @@
 import RigDashboardNavbar from "./RigDashboardNavbar";
 import "./RigDashboard.css";
 import RigDashboardContents from "./RigDashboardContents";
+import { useEffect } from "react";
+import axios from "axios";
 
 function RigDashboardRoot() {
+    // Get updates once Dashboard has loaded
+    useEffect(() => {
+        axios.get("http://localhost:5000/getUpdates").then((response) => {
+            if (response.data !== process.env.REACT_APP_VERSION) {
+                alert(`Updating Rig to v${response.data}`)
+            }
+        })
+    }, []);
+
     return (
         <div className="rig-dashboard">
             <RigDashboardNavbar />
